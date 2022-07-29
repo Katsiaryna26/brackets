daf943442fdd3c3dccaf7c704277e36ba240d28b
Katsiaryna26 <mary260188@mail.ru>
2022-07-29 16:20:11 +0300

    feat:create a parenthesis checking function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+for (let  i = 0; i < str.length; i++){
+  bracketsConfig.forEach(item=> {if (str[i] === item[0] && str[i+1] === item[1]) {
+  str = str.slice(0,i) + str.slice(i+2, str.length);
+  i-=2;
+}
+ });
+}
+
+
+return (str.length === 0);
 }
